8c0db7fac8c47a058ca6d2497739c0fdf67845ab
Vojislav <vojislav.sibinovic@gmail.com>
2020-12-22 14:52:04 +0100

    added boards model to the backend

--- server/src/services/boards/boards.hooks.js
+++ server/src/services/boards/boards.hooks.js
@@ -1,14 +1,45 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const { setField } = require('feathers-authentication-hooks');
 
 module.exports = {
   before: {
     all: [ authenticate('jwt') ],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    find: [
+      setField({
+        from: 'params.user.id',
+        as: 'params.query.ownerId'
+      })
+    ],
+    get: [
+      setField({
+        from: 'params.user.id',
+        as: 'params.query.ownerId'
+      })
+    ],
+    create: [
+      setField({
+        from: 'params.user.id',
+        as: 'data.sentBy'
+      })
+    ],
+    update: [
+      setField({
+        from: 'params.user.id',
+        as: 'params.query.ownerId'
+      })
+    ],
+    patch: [
+      setField({
+        from: 'params.user.id',
+        as: 'params.query.ownerId'
+      })
+    ],
+    remove: [
+      setField({
+        from: 'params.user.id',
+        as: 'params.query.ownerId'
+      })
+    ]
   },
 
   after: {
