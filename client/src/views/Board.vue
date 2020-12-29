25980e164a49973eebd4b334b944f1e2556b5e3d
Vojislav <vojislav.sibinovic@gmail.com>
2020-12-29 19:41:36 +0100

    Activity bard started, task service added

--- client/src/views/Board.vue
+++ client/src/views/Board.vue
@@ -1,80 +1,98 @@
 <template>
   <v-container flud>
     <v-slide-y-transition mode="out-in">
-      <v-layout class="d-flex column mb-6">
-        <v-progress-circular
-          v-if="loadingBoard || loadingLists"
-          :size="70"
-          :width="7"
-          indeterminate
-          color="primary"
-        >
-        </v-progress-circular>
-        <div v-if="boardsError">
-          <v-alert type="error" :value="boardsError">
-            {{ boardsError.message }}
-          </v-alert>
-        </div>
-        <h2 v-if="board">{{ board.name }}</h2>
-        <div v-if="!loadingLists" class="d-flex">
-          <v-card
-            max-width="374"
-            v-for="list in lists"
-            :key="list._id"
-            @dragover="setDroppingList($event, list)"
-            :class="{ green: droppingList == list }"
-          >
-            <v-card-title>{{ list.name }}</v-card-title>
-            <v-col>
-              <v-card>
-                <ul v-if="cardsByListId[list._id]">
-                  <v-card
-                    v-for="card in cardsByListId[list._id]"
-                    :key="card._id"
-                    draggable="true"
-                    @dragstart="startDraggingCard(card)"
-                    @dragend="dropCard()"
-                  >
-                    {{ card.title }}
-                  </v-card>
-                </ul>
-              </v-card>
-            </v-col>
-            <create-card
-              :boardId="$route.params.id"
-              :listId="list._id"
-            ></create-card>
-          </v-card>
-          <v-card max-width="344">
-            <v-card-title>Create List</v-card-title>
-            <v-form
-              v-model="validList"
-              @submit.prevent="createList"
-              @keydown.prevent.enter
-              v-if="!creatingList"
+      <v-layout>
+        <v-flex xs10>
+          <v-layout class="d-flex column mb-6">
+            <v-progress-circular
+              v-if="loadingBoard || loadingLists"
+              :size="70"
+              :width="7"
+              indeterminate
+              color="primary"
             >
-              <v-layout column align-center>
-                <v-col cols="12">
-                  <v-text-field
-                    v-model="list.name"
-                    :rules="notEmptyRules"
-                    label="Name"
-                    required
-                  ></v-text-field>
+            </v-progress-circular>
+            <div v-if="boardsError">
+              <v-alert type="error" :value="boardsError">
+                {{ boardsError.message }}
+              </v-alert>
+            </div>
+            <h2 v-if="board">{{ board.name }}</h2>
+            <div v-if="!loadingLists" class="d-flex">
+              <v-card
+                max-width="374"
+                v-for="list in lists"
+                :key="list._id"
+                @dragover="setDroppingList($event, list)"
+                :class="{ green: droppingList == list }"
+              >
+                <v-card-title>{{ list.name }}</v-card-title>
+                <v-col>
+                  <v-card>
+                    <ul v-if="cardsByListId[list._id]">
+                      <v-card
+                        v-for="card in cardsByListId[list._id]"
+                        :key="card._id"
+                        draggable="true"
+                        @dragstart="startDraggingCard(card)"
+                        @dragend="dropCard()"
+                      >
+                        {{ card.title }}
+                      </v-card>
+                    </ul>
+                  </v-card>
                 </v-col>
-                <v-btn type="submit" :disabled="!validList">Create List</v-btn>
-                <v-progress-circular
-                  v-if="creatingList"
-                  :size="70"
-                  :width="7"
-                  indeterminate
-                  color="primary"
+                <create-card
+                  :boardId="$route.params.id"
+                  :listId="list._id"
+                ></create-card>
+              </v-card>
+              <v-card max-width="344">
+                <v-card-title>Create List</v-card-title>
+                <v-form
+                  v-model="validList"
+                  @submit.prevent="createList"
+                  @keydown.prevent.enter
+                  v-if="!creatingList"
                 >
-                </v-progress-circular>
-              </v-layout>
-            </v-form>
+                  <v-layout column align-center>
+                    <v-col cols="12">
+                      <v-text-field
+                        v-model="list.name"
+                        :rules="notEmptyRules"
+                        label="Name"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-btn type="submit" :disabled="!validList"
+                      >Create List</v-btn
+                    >
+                    <v-progress-circular
+                      v-if="creatingList"
+                      :size="70"
+                      :width="7"
+                      indeterminate
+                      color="primary"
+                    >
+                    </v-progress-circular>
+                  </v-layout>
+                </v-form>
+              </v-card>
+            </div>
+          </v-layout>
+        </v-flex>
+        <v-flex xs2>
+          <v-card>
+            <v-card-title>Tasks</v-card-title>
+            <v-list v-for="task in activityLog" :key="task._id" class="pa-2">
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ task.text }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list>
           </v-card>
-        </div>
+        </v-flex>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
@@ -119,19 +137,31 @@ export default {
     }).then((response) => {
       const cards = response.data || response;
     });
+    this.findTasks({
+      query: {
+        boardId: this.$route.params.id,
+      },
+    }).then((response) => {
+      const tasks = response.data || response;
+    });
   },
   methods: {
     ...mapActions("boards", { getBoard: "get" }),
     ...mapActions("lists", { findLists: "find" }),
     ...mapActions("cards", { findCards: "find" }),
-    createList() {
+    ...mapActions("tasks", { findTasks: "find" }),
+    async createList() {
       if (this.validList) {
         //console.log(this.list);
-        const { Lists } = this.$FeathersVuex.api;
+        const { Lists, Tasks } = this.$FeathersVuex.api;
         this.list.boardId = this.$route.params.id;
         const list = new Lists(this.list);
 
-        list.save();
+        await list.save();
+        const task = new Tasks();
+        task.text = `${this.user.user.displayName} created list ${list.name}`;
+        task.boardId = this.$route.params.id;
+        task.save();
       }
     },
     startDraggingCard(card) {
@@ -161,11 +191,20 @@ export default {
       creatingList: "isCreatePending",
       listsError: "errorOnFind",
     }),
+    ...mapState("auth", { user: "payload" }),
     ...mapState("cards", {
       cardsError: "errorOnFind",
     }),
     ...mapGetters("lists", { findListsInStore: "find" }),
     ...mapGetters("cards", { findCardsInStore: "find" }),
+    ...mapGetters("tasks", { findTasksInStore: "find" }),
+    tasks() {
+      return this.findTasksInStore({
+        query: {
+          boardId: this.$route.params.id,
+        },
+      }).data;
+    },
     lists() {
       return this.findListsInStore({
         query: {
@@ -186,6 +225,9 @@ export default {
         byId[card.listId].push(card);
         return byId;
       }, {});
+    },
+    activityLog() {
+      return this.tasks.slice().reverse();
     },
   },
 };
